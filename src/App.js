f274519bed87f3f785ff62bb90e3bc0463b003cf
akash768145s <akash2210476@ssn.edu.in>
2024-11-12 23:13:03 +0530

    deployapi Proper

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
     if (!hashParams.access_token) {
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://mood-sync2.vercel.app/callback';
+        'https://accounts.spotify.com/authorize?client_id=6730daef9d964827a24312dc54ccdfe8&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://mood-sync2.vercel.app/callback';
     } else {
       this.props.setToken(hashParams.access_token);
     }
